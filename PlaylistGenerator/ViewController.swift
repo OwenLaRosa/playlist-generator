bb4d766e093258a9c2c3dd181c2eced7de66a27e
OwenLaRosa <owen.larosa@outlook.com>
2016-06-04 16:06:09 -0400

    First Commit

---
+++ PlaylistGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlaylistGenerator
+//
+//  Created by Owen LaRosa on 6/4/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
