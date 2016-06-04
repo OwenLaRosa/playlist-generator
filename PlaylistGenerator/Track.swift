7c5d3357bc8ce27ba890fe8ff58e3cb648d178d5
OwenLaRosa <owen.larosa@outlook.com>
2016-06-04 16:20:04 -0400

    Add class for Track entity

---
+++ PlaylistGenerator/Track.swift
@@ -0,0 +1,38 @@
+//
+//  Track.swift
+//  PlaylistGenerator
+//
+//  Created by Owen LaRosa on 6/4/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import CoreData
+
+class Track: NSManagedObject {
+    
+    @NSManaged var title: String
+    @NSManaged var artist: String
+    @NSManaged var type: String
+    @NSManaged var new: Bool
+    @NSManaged var firstAdded: NSDate
+    @NSManaged var lastPlayed: NSDate?
+    @NSManaged var becomesOld: NSDate?
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(title: String, artist: String, new: Bool, becomesOld: NSDate?, context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entityForName("Track", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        self.title = title
+        self.artist = artist
+        self.new = new
+        self.becomesOld = becomesOld
+        self.type = ""
+        self.lastPlayed = nil
+        self.firstAdded = NSDate()
+    }
+    
+}
