5ef00ebd3a29ee79928c795d0af7d637238d27a8
OwenLaRosa <owen.larosa@outlook.com>
2016-06-05 12:05:32 -0400

    Populate table with track entities

--- PlaylistGenerator/LibraryViewController.swift
+++ PlaylistGenerator/LibraryViewController.swift
@@ -11,11 +11,15 @@ import Cocoa
 class LibraryViewController: NSViewController {
     
     @IBOutlet weak var tableView: NSTableView!
+    
+    var tracks = [Track]()
+    let context = (NSApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let fetchRequest = NSFetchRequest(entityName: "Track")
+        tracks = try! context.executeFetchRequest(fetchRequest) as! [Track]
     }
 
     override var representedObject: AnyObject? {
@@ -29,7 +33,7 @@ class LibraryViewController: NSViewController {
 extension LibraryViewController: NSTableViewDataSource {
     
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
-        return 30
+        return tracks.count
     }
     
 }
@@ -38,9 +42,17 @@ extension LibraryViewController: NSTableViewDelegate {
     
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
         
-        if let cell = tableView.makeViewWithIdentifier("TableCell", owner: nil) as? NSTableCellView {
-            cell.textField?.stringValue = ""
-            return cell
+        let track = tracks[row]
+        guard let cell = tableView.makeViewWithIdentifier("TableCell", owner: nil) as? NSTableCellView else {
+            return nil
+        }
+        
+        if tableColumn == tableView.tableColumns[0] {
+            cell.textField?.stringValue = track.title
+        } else if tableColumn == tableView.tableColumns[1] {
+            cell.textField?.stringValue = track.artist
+        } else if tableColumn == tableView.tableColumns[2] {
+            cell.textField?.stringValue = track.type
         }
         return nil
     }
