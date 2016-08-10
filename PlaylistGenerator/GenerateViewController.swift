31c364a9d2a3784f1116a6c146ae2bf2356c8b12
OwenLaRosa <owen.larosa@outlook.com>
2016-08-10 11:17:25 -0400

    feat: Populate table view

--- PlaylistGenerator/GenerateViewController.swift
+++ PlaylistGenerator/GenerateViewController.swift
@@ -16,7 +16,7 @@ class GenerateViewController: NSViewController {
     
     @IBOutlet weak var typePopupButton: NSPopUpButton!
     
-    var selectedTypes = [Category]()
+    var selectedTypeNames = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +31,10 @@ class GenerateViewController: NSViewController {
     }
     
     @IBAction func addButtonClicked(sender: NSButton) {
-        
+        if let title = typePopupButton.titleOfSelectedItem {
+            selectedTypeNames.append(title)
+            tableView.reloadData()
+        }
     }
     
     @IBAction func generateButtonClicked(sender: NSButton) {
@@ -40,6 +43,29 @@ class GenerateViewController: NSViewController {
     
     @IBAction func cancelButtonClicked(sender: NSButton) {
         dismissController(nil)
+    }
+    
+}
+
+extension GenerateViewController: NSTableViewDataSource {
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return selectedTypeNames.count
+    }
+    
+}
+
+extension GenerateViewController: NSTableViewDelegate {
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        print("viewForTableColumn")
+        guard let cell = tableView.makeViewWithIdentifier("TypeCell", owner: nil) as? NSTableCellView else {
+            return nil
+        }
+        
+        cell.textField?.stringValue = selectedTypeNames[row]
+        
+        return cell
     }
     
 }
