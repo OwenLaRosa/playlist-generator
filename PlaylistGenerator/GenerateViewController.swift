a7968a01f11dfd20db849f7b8d6eb14fa467ee66
OwenLaRosa <owen.larosa@outlook.com>
2016-08-10 10:46:02 -0400

    feat: Populate category menu, implement cancel

--- PlaylistGenerator/GenerateViewController.swift
+++ PlaylistGenerator/GenerateViewController.swift
@@ -10,17 +10,36 @@ import Cocoa
 
 class GenerateViewController: NSViewController {
     
+    let context = (NSApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
     @IBOutlet weak var tableView: NSTableView!
     
-    @IBOutlet weak var typeComboBox: NSComboBox!
+    @IBOutlet weak var typePopupButton: NSPopUpButton!
+    
+    var selectedTypes = [Category]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let typeFetchRequest = NSFetchRequest(entityName: "Category")
+        var typeNames = try! context.executeFetchRequest(typeFetchRequest).map() {
+            ($0 as! Category).name
+        }
+        typeNames.append("New")
+        typePopupButton.removeAllItems()
+        typePopupButton.addItemsWithTitles(typeNames)
+    }
     
     @IBAction func addButtonClicked(sender: NSButton) {
+        
     }
     
     @IBAction func generateButtonClicked(sender: NSButton) {
+        
     }
     
     @IBAction func cancelButtonClicked(sender: NSButton) {
+        dismissController(nil)
     }
     
 }
