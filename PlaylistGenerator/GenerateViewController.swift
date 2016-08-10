3365c18c7a48301ffe76207de4b2f48b74e91bdc
OwenLaRosa <owen.larosa@outlook.com>
2016-08-10 10:23:04 -0400

    feat: Add view controller to generate playlist

---
+++ PlaylistGenerator/GenerateViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GenerateViewController.swift
+//  PlaylistGenerator
+//
+//  Created by Owen LaRosa on 8/10/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Cocoa
+
+class GenerateViewController: NSViewController {
+    
+    @IBOutlet weak var tableView: NSTableView!
+    
+    @IBOutlet weak var typeComboBox: NSComboBox!
+    
+    @IBAction func addButtonClicked(sender: NSButton) {
+    }
+    
+    @IBAction func generateButtonClicked(sender: NSButton) {
+    }
+    
+    @IBAction func cancelButtonClicked(sender: NSButton) {
+    }
+    
+}
