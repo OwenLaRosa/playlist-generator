42b1af5aa2544031883bc433ce59c13cb13e741e
OwenLaRosa <owen.larosa@outlook.com>
2016-09-07 17:35:26 -0400

    feat: Add logic to generate playlist

--- PlaylistGenerator/GenerateViewController.swift
+++ PlaylistGenerator/GenerateViewController.swift
@@ -39,13 +39,108 @@ class GenerateViewController: NSViewController {
     }
     
     @IBAction func generateButtonClicked(sender: NSButton) {
-        
+        generatePlaylist()
     }
     
     @IBAction func cancelButtonClicked(sender: NSButton) {
         dismissController(nil)
     }
     
+    func generatePlaylist() {
+        var playlistItems = [String]()
+        var usedArtists = [Artist]()
+        
+        for i in selectedTypeNames {
+            print(i)
+        }
+        
+        var artists = getArtists()
+        var categoryTable = getCategories()
+        var newTracks = getNewTracks()
+        print("artists: \(artists.count)")
+        print("newTracks: \(newTracks.count)")
+        for i in selectedTypeNames {
+            if i == "New" {
+                if newTracks.count == 0 {
+                    playlistItems.append("No Track")
+                } else {
+                    let availableTracks = newTracks.filter() {
+                        !usedArtists.contains($0.artist)
+                    }
+                    if availableTracks.count == 0 {
+                        playlistItems.append("No Track")
+                    } else {
+                        let track = availableTracks[Int(arc4random()) % availableTracks.count]
+                        playlistItems.append("\(track.title) - \(track.artist.name)")
+                        usedArtists.append(track.artist)
+                    }
+                }
+            } else if i == "Artist" {
+                let availableArtists = artists.filter() {
+                    !usedArtists.contains($0)
+                }
+                if availableArtists.count == 0 {
+                    playlistItems.append("No Artist")
+                } else {
+                    let artist = availableArtists[Int(arc4random()) % availableArtists.count]
+                    playlistItems.append("Song by \(artist.name)")
+                    usedArtists.append(artist)
+                }
+            } else {
+                if let availableTracks = categoryTable[i]?.tracks.filter({
+                    !usedArtists.contains($0.artist)
+                }) {
+                    if availableTracks.count == 0 {
+                        let track = availableTracks[Int(arc4random()) % availableTracks.count]
+                        playlistItems.append("\(track.title) - \(track.artist.name)")
+                        usedArtists.append(track.artist)
+                    } else {
+                        playlistItems.append("No Track")
+                    }
+                } else {
+                    playlistItems.append("No Track")
+                }
+            
+            }
+        }
+        print("finished generating playlist")
+        for i in playlistItems {
+            print(i)
+        }
+    }
+    
+    // MARK: - Fetch Requests
+    
+    func getArtists() -> [Artist] {
+        let fetchRequest = NSFetchRequest(entityName: "Artist")
+        return try! context.executeFetchRequest(fetchRequest) as! [Artist]
+    }
+    
+    func getCategories() -> [String: Category] {
+        let fetchRequest = NSFetchRequest(entityName: "Category")
+        let categories = try! context.executeFetchRequest(fetchRequest) as! [Category]
+        var categoryTable = [String: Category]()
+        for i in categories {
+            categoryTable[i.name] = i
+        }
+        return categoryTable
+    }
+    
+    func getNewTracks() -> [Track] {
+        let fetchRequest = NSFetchRequest(entityName: "Track")
+        fetchRequest.predicate = NSPredicate(format: "new == true", argumentArray: nil)
+        let fetchedObjects = try! context.executeFetchRequest(fetchRequest) as! [Track]
+        var result = [Track]()
+        for i in fetchedObjects {
+            if NSDate().timeIntervalSinceDate(i.becomesOld!) > 0 {
+                i.new = false
+                continue
+            }
+            result.append(i)
+        }
+        return result
+    }
+        
 }
 
 extension GenerateViewController: NSTableViewDataSource {
